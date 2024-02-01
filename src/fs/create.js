350dbb2ebdf994959b79f4ac415e9d01880e7f02
annatvali <anntvaliashvili@gmail.com>
2024-02-01 18:50:10 +0400

    fix: write create.js with  AMD format

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,12 +1,22 @@
-const create = async () => {
-  const fs = require('node:fs');
-  const content = 'I am fresh and young';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-  fs.writeFile('./fresh.txt', content, (err) => {
-    if (err) {
-      console.log(err);
+const create = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const newFile = path.join(__dirname, 'files', 'fresh.txt');
+  try {
+    await fs.access(newFile);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await fs.writeFile(newFile, 'I am fresh and young');
+      console.log('File successfully created and written to');
+    } else {
+      throw err;
     }
-  });
+  }
 };
 
-await create();
+await create().catch((error) => console.error(error));
